a06c703e6ce832f4b9b38ffc68bec9605d02bf15
Greg Mercer <gmercer@stanford.edu>
2014-06-19 08:26:43 -0700

    Updated to output html

--- logs.sh
+++ logs.sh
@@ -3,20 +3,24 @@
 # run this script from the build directory
 
 function gitlog() {
-	echo "        "
-	echo "        "
-	echo "*-----------------------------------------*"
-	echo "$1"
-	echo "*-----------------------------------------*"
-	echo "        "
-	# git log --pretty=format:"%h - %an, %ar : %s" --committer "Greg Mercer" --stat --color --since=1.days
-	git log --all --pretty=format:"https://github.com/gsbitse/$1/commit/%h%n- committer: %cn author: %an, %ar %n%s" --stat --color --since=$2.days
+  echo "<p>"   
+  echo "==========================================="
+  echo "</br>"  
+	echo "<strong>$1</strong>"
+  echo "</br>"  
+  echo "==========================================="
+  git config --global alias.lg "log --all --pretty=format:'</br></br><a href="https://github.com/gsbitse/$1/commit/%h%n">view commit</a> - committer: %cn author: %an, %ar</br>%b %n%N'" 
+  git lg --stat --since=$2.days
+  echo "</br>"
+  echo "</p>" 
 }
 
 function messageout() {
+  echo "<p>" 
 	echo "    "
 	echo "    "
-	echo "$1"	
+	echo "$1"
+  echo "</p>"	
 }
 
 function getlogs() {
@@ -36,6 +40,21 @@ function getlogs() {
   done  
 }
 
+echo '<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+
+    <!--[if lt IE 8]>
+        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->'
+
 messageout "!!! status: logs.sh started..."
 
 cd docroot/profiles/gsb_public
@@ -50,3 +69,9 @@ cd ../../../themes
 getlogs $1
 
 messageout "!!! status: logs.sh completed."
+
+echo '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  </body>
+</html>' 
+
+
