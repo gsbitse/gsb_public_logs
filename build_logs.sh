1316466444525a263a602a2b794c95213dd59b6e
Greg Mercer <gmercer@stanford.edu>
2014-06-21 16:34:29 -0700

    Changing to release-4.0

--- build_logs.sh
+++ build_logs.sh
@@ -9,7 +9,7 @@ distro_url="https://github.com/gsbitse/gsb-distro.git"
 # save the workspace root directory
 
 workspace_dir=$PWD
-branch="release-3.12"
+branch="release-4.0"
 
 ############################################
 # check if the gsb-distro branch exists
