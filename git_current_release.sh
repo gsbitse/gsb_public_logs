c553fe6f28132e0f591c45d38b76c5652918648c
Greg Mercer <gmercer@stanford.edu>
2015-03-14 02:35:49 -0700

    Update git_current_release.sh

--- git_current_release.sh
+++ git_current_release.sh
@@ -4,3 +4,4 @@ rm -rf gsb_public
 git clone git@github.com:gsb-public/gsb_public.git
 cd gsb_public
 git for-each-ref --sort=-committerdate --format='%(refname:short)' | grep release | cut -c8- | awk 'NR==1'
+cd ..
