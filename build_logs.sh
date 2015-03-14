51fe8b01df41537e68d6d092187fc9d26c5f4adc
Greg Mercer <gmercer@stanford.edu>
2015-03-14 02:39:54 -0700

    Update build_logs.sh

--- build_logs.sh
+++ build_logs.sh
@@ -9,7 +9,7 @@ distro_url="https://github.com/gsbitse/gsb-distro.git"
 # save the workspace root directory
 
 workspace_dir=$PWD
-branch="release-5.07"
+branch="$1"
 
 ############################################
 # check if the gsb-distro branch exists
